e1675c58269ea95e8f6349564ac46d9a2bc2d431
steventapiero24 <steventapiero24@gmail.com>
2025-03-15 15:19:30 -0500

    create component awards

---
+++ src/app/awards/awards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-awards',
+  imports: [],
+  templateUrl: './awards.component.html',
+  styleUrl: './awards.component.css'
+})
+export class AwardsComponent {
+
+}
