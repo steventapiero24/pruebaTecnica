48aa6783bc145bcd16876c03f7643b0a7b186365
steventapiero24 <steventapiero24@gmail.com>
2025-03-15 12:13:33 -0500

    Nav and Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, BreadcrumbComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from  '@angular/material/icon' ;
+import {MatButtonModule} from  '@angular/material/button' ;
+import {MatToolbarModule} from  '@angular/material/toolbar' ;
+
+export class AppModule { }
+
 
 @Component({
   selector: 'app-nav',
   standalone: true, 
+  imports : [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
