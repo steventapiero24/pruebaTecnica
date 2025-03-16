2f8a4909e02e8c778b0766c3b4edacf3b6dbcdd6
steventapiero24 <steventapiero24@gmail.com>
2025-03-16 10:03:24 -0500

    about us component maqueta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServiciosComponent } from './servicios/servicios.component';
 import { AwardsComponent } from "./awards/awards.component";
 import { BannerComponent } from "./banner/banner.component";
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, BreadcrumbComponent, HeaderComponent, ServiciosComponent, AwardsComponent, BannerComponent],
+  imports: [RouterOutlet, NavComponent, BreadcrumbComponent, HeaderComponent, ServiciosComponent, AwardsComponent, BannerComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
